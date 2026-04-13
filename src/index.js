29fc94813e988a728e02cf3d27bb1fec8ade9dae
vutii <giftmuuo.com@gmail.com>
2026-04-13 18:30:31 +0300

    update

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CartProvider } from './Addcart';
-import { HashRouter } from 'react-router-dom'; // ✅ ADD THIS
+import { CartProvider } from './Addcart'; // ✅ MATCH EXACT FILE NAME
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter> {/* ✅ ADD THIS */}
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </HashRouter>
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>
 );
 
